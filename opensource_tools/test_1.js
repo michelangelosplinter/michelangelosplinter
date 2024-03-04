9a9f266792283bfd39397df456324aed4be5498c
michelangelosplinter <143991999+michelangelosplinter@users.noreply.github.com>
2024-03-04 22:32:46 +0200

    Add files via upload

---
+++ opensource_tools/test_1.js
@@ -0,0 +1,6 @@
+// Calllinnnnn a Powergell purcess
+var spawn = require("child_process").spawn;
+spawn("powershell.exe",[".\\psServer.ps1 -username madorcyber@gmail.com -password Sitni16092019"]);
+
+/* Fuck thuis ba·guette /ᵇᵃˈᵍᵉᵗ/ */
+
